f2f7d85a2982d043309934f09448228d4221104b
Kaleo A. Piasse <kaleopiasse@gmail.com>
2017-09-02 15:52:07 -0300

    First commit test class Person

---
+++ src/projectacademylp3/ProjectAcademyLP3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectacademylp3;
+
+/**
+ *
+ * @author kaleo
+ */
+public class ProjectAcademyLP3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Person p1 = new Person("39958446855", "Kaleo Aparecido Piasse");
+        
+        System.out.println(p1);
+    }
+    
+}

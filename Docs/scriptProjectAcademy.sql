a02fe62860d51fd2f3208e37d962a8315be1f106
Kaleo Piasse <kaleo.piasse@gmail.com>
2017-10-13 18:08:39 -0300

    Script Banco de Dados

---
+++ Docs/scriptProjectAcademy.sql
@@ -0,0 +1,18 @@
+create database ProjectAcademy;
+
+use ProjectAcademy;
+
+create table person ( 
+cpf varchar (11), 
+name varchar(50), 
+sex varchar(12), 
+street varchar(30), 
+district varchar(30), 
+cep varchar(9), 
+city varchar(30),  
+estado varchar(2),
+primary key (cpf));
+
+
+//sites uteis
+// https://pt.icons8.com/icon/set/login/win8

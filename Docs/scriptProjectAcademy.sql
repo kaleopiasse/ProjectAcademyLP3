a8bc6e918714e7910e3df176652e99554514a6cd
Kaleo Piasse <kaleo.piasse@gmail.com>
2017-10-23 17:50:09 -0200

    Alteração Script

--- Docs/scriptProjectAcademy.sql
+++ Docs/scriptProjectAcademy.sql
@@ -43,10 +43,10 @@ foreign key (cpf_student) references person (cpf));
 
 create table monthly (
 cpf_student varchar(11),
-date_maturity date,
-price float,
-type_plan varchar(50),
-primary key (cpf_student, date_maturity),
+date date,
+price decimal(10,2),
+plan varchar(50),
+primary key (cpf_student, date),
 foreign key (cpf_student) references student (cpf_student));
 
 create table person_email(
@@ -61,7 +61,22 @@ phone varchar(30),
 primary key (cpf_phone, phone),
 foreign key (cpf_phone) references person (cpf));
 
+insert into person (cpf,name,sex,street,district,num,cep,city,estado,complement) 
+	   values ("39958446855","Kaleo Aparecido Piasse","Masculino","Avenida Perimetral","Jardim Industrial",1031,"13571385","São Carlos","SP","Bloco 27 apto 502"),
+			  ("39958446856","Son Goku","Masculino","Distrito Leste","Distrito Leste",439,"13571386","terra","SP","loucura"),
+			  ("39958446857","Uzumaki Naruto","Masculino","Rua dos Ninjas","Area Sul",566,"13571387","Konoha Sempu","SP","apto 502"),
+			  ("39958446858","Light Yagami","Masculino","Daikoku","Centro",777,"13571388","To-Ch","RJ","nenhum"),
+			  ("39958446859","Edward Elric","Masculino","Resembool","Campo",888,"13571389","Amestris","TO","nenhum");
 
+insert into student (cpf_student, situation) 
+	   values ("39958446855","Ativo"),
+	          ("39958446856","Ativo"),
+			  ("39958446857","Ativo"),
+			  ("39958446858","Bloqueado"),
+			  ("39958446859","Inativo");
+
+insert into modality (name,price) values ("Muay Thai",75.00), ("Kung Fu",75.00), ("Boxe",75.00), ("Musculação",65.00), ("MMA",95.00);
 
 //sites uteis
 // https://pt.icons8.com/icon/set/login/win8
+// https://www.mockaroo.com/
